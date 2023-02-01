72553009106c65f87624568ccebb4474fc36711c
BGAlexandru <gabriel.belecciu@365.univ-ovidius.ro>
2023-02-01 18:20:38 +0200

    first commit

--- .batch_script/delete_depedencies.bat
+++ .batch_script/delete_depedencies.bat
@@ -2,10 +2,12 @@
 
 setlocal enabledelayedexpansion
 
+for %%* in (.) do set current_path=%%~dpn*
+
 set /a x=0
 for /F "delims=" %%p in (depedencies.txt) do (
-	IF exist "..\%%p\node_modules\" (
-		rd /s /q "..\%%p\node_modules"
+	IF exist "%current_path%%%p\node_modules\" (
+		rd /s /q "%current_path%%%p\node_modules"
 		echo %%p depedencies succesfully deleted.
 	) ELSE (
 		echo %%p depedencies don't exist.
--- .batch_script/install_depedencies.bat
+++ .batch_script/install_depedencies.bat
@@ -2,13 +2,15 @@
 
 setlocal enabledelayedexpansion
 
+for %%* in (.) do set current_path=%%~dpn*
+
 set /a x=0
 for /F "delims=" %%p in (depedencies.txt) do (
-	IF exist "..\%%p\node_modules\" (
+	IF exist "%current_path%%%p\node_modules\" (
 		echo %%p depedencies already exists.
 	) ELSE (
-		call npm install --prefix "..\%%p" package.json
-		call npm audit --prefix "..\%%p" fix --force
+		call npm install --prefix "%current_path%%%p" package.json
+		call npm audit --prefix "%current_path%%%p" fix --force
 		echo %%p depedencies succesfully installed.
 	)
 )
